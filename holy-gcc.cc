2202a15d68d112d4789f00317b3aa3abc8da52b8
YishaiZinkin <yishaizinkin@gmail.com>
2020-04-21 19:51:53 +0300

    Modified the plugin to actually support HolyC prints

--- holy-gcc.cc
+++ holy-gcc.cc
@@ -23,32 +23,61 @@ is_string_cst_addr (tree t)
          (TREE_CODE (TREE_OPERAND (t, 0)) == STRING_CST);
 }
 
+static bool
+is_holy_c_print (tree t)
+{
+  /* What we're looking for is a statement that contains only an address of
+     string litaral being casted to (char *). I.e. something like that:
+     (char *) "some string";
+     In other words, we're looking for a statement which is a pointer cast
+     (NOP_EXPR), that casts to (char *) and takes a STRING_CST as an
+     operand.  */
+  return TREE_CODE (t) == NOP_EXPR &&
+         is_pointer_to_char_type (TREE_TYPE (t)) &&
+         is_string_cst_addr (TREE_OPERAND (t, 0));
+}
+
+static tree
+build_print_call_expr (tree str)
+{
+  tree fn = builtin_decl_implicit (BUILT_IN_PRINTF);
+  return build_call_expr (fn, 1, str);
+}
 
 static tree
 cb_walk_tree_fn (tree *tp, int *walk_subtrees,
      void *data ATTRIBUTE_UNUSED)
 {
-  /* TODO: Is it possible to get STATEMENT_LIST out of fndecl directly,
-     withuot walk_tree?  */
+  /* TODO: Is it possible to get all STATEMENT_LISTs out of fndecl directly,
+     withuot walk_tree the entire function?  */
   if (TREE_CODE (*tp) == STATEMENT_LIST)
   {
-    tree_stmt_iterator i;
-    for (i = tsi_start (*tp); !tsi_end_p (i); tsi_next (&i))
+    tree_stmt_iterator tsi = tsi_start (*tp);
+    while (!tsi_end_p (tsi))
     {
-      tree stmt = tsi_stmt (i);
-      /* What we're looking for is a statement that contains only an address of
-         string litaral being casted to (char *). I.e. something like that:
-         (char *) "Some string";
-         In other words, we're looking for a statement which is a pointer cast
-         (NOP_EXPR), that casts to a (char *) and takes a STRING_CST as an
-         operand.  */
-      if (TREE_CODE (stmt) == NOP_EXPR &&
-          is_pointer_to_char_type (TREE_TYPE (stmt)) &&
-          is_string_cst_addr (TREE_OPERAND (stmt, 0)))
+      tree stmt = tsi_stmt (tsi);
+      if (is_holy_c_print (stmt))
       {
-        inform (tree_nonartificial_location (stmt), "HolyC print");
+        /* Replace this expression with a call to a print function and pass
+           it as an argument  */
+        tsi_link_before (&tsi, build_print_call_expr (stmt), TSI_SAME_STMT);
+        tsi_delink (&tsi);
+      }
+      else
+      {
+        /* If we performed tsi_delink() tsi already points to the next
+           statement  */
+        tsi_next (&tsi);
       }
     }
+  }
+  /* This is a special case. If a BIND_EXPR contains only one statement,
+     instead of holding a STATEMENT_LIST of one element it's replaced with that
+     single statement  */
+  else if (TREE_CODE (*tp) == BIND_EXPR &&
+           is_holy_c_print (BIND_EXPR_BODY (*tp)))
+  {
+    BIND_EXPR_BODY (*tp) = build_print_call_expr (BIND_EXPR_BODY (*tp));
   }
 
   return NULL_TREE;
